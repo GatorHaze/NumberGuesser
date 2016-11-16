5ed9755c6005eeda46674f15616c0575b9763637
Justin <justhaze08@gmail.com>
2016-11-15 19:34:06 -0500

    Still need to clean up the formatting

--- Program.cs
+++ Program.cs
@@ -13,10 +13,48 @@ namespace numberguesser
             Random randomNum = new Random();
             int getRandomNumber = randomNum.Next(0, 100);
             Console.WriteLine($"The number {getRandomNumber} was generated!");
-            
 
-            Console.WriteLine("Please enter a random number between 1 and 100");
-            var input = Console.ReadLine();
+
+            var num = 0;
+            var attempts = 0;
+            var lastNum = 0;
+            var maxtries = 5;
+
+
+            while (attempts < maxtries && getRandomNumber != num)
+            {
+                lastNum = num;
+                Console.WriteLine("Please enter a random number between 1 and 100");
+                var newNum = int.TryParse(Console.ReadLine(), out num);
+                attempts++;
+
+
+                if (attempts >= maxtries)
+                {
+                    Console.WriteLine("Your tries are up, and you Lose");
+                    Console.ReadLine();
+
+                }
+
+                else if (num == getRandomNumber)
+                {
+                    Console.WriteLine("Awesome! You WIN!!!");
+                    Console.ReadLine();
+                }
+
+                if (lastNum > getRandomNumber && num > lastNum)
+                { Console.WriteLine("Still need to go lower!"); }
+
+
+                else if (lastNum < getRandomNumber)
+                {
+                    Console.WriteLine($"Fell a little short, you need to go higher");
+                }
+                else if (lastNum > getRandomNumber)
+                {
+                    Console.WriteLine($"{num} is too high, calm down!");
+                }
+            }
         }
     }
 }
